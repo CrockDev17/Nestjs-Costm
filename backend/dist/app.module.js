9ff1bb5f01b78573b1a9190a869b54bb1432e4f9
Sitraka Lalaina <sitrakalembason@gmail.com>
2026-09-26 16:25:57 +0300

    vérification finale

--- backend/dist/app.module.js
+++ backend/dist/app.module.js
@@ -12,6 +12,7 @@ const typeorm_1 = require("@nestjs/typeorm");
 const typeorm_config_1 = require("./core/config/typeorm.config");
 const etudiant_module_js_1 = require("./modules/etudiant/etudiant.module.js");
 const presence_module_js_1 = require("./modules/presence/presence.module.js");
+const programme_module_js_1 = require("./modules/programme/programme.module.js");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -21,6 +22,7 @@ exports.AppModule = AppModule = __decorate([
             typeorm_1.TypeOrmModule.forRoot(typeorm_config_1.typeOrmConfig),
             etudiant_module_js_1.EtudiantModule,
             presence_module_js_1.PresenceModule,
+            programme_module_js_1.ProgrammeModule,
         ],
     })
 ], AppModule);
